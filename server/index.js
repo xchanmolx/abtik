a5e9302b976aa6e335699d469fad9115c9ac37f9
xchanmolx <cmanigos@gmail.com>
2019-12-24 10:58:58 +0800

    setup backend

---
+++ server/index.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const db = require('./database');
+
+const ENV = process.env.NODE_ENV;
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use('/api/cities', require('./api/cities'));
+app.use('/api/weather', require('./api/weather'));
+
+if (ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use((req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+}
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}!`);
+});
+
+db.query('SELECT NOW()', (err, res) => {
+    if (err.error)
+      return console.log(err.error);
+    console.log(`PostgreSQL connected: ${res[0].now}.`);
+});
+
+module.exports = app;
---
+++ server/models/weather.js
@@ -0,0 +1,19 @@
+const request = require('request-promise');
+
+require('dotenv').config();
+
+class Weather {
+    static retrieveByCity (city, callback) {
+      request({
+        uri: `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${process.env.WEATHER_API_KEY}&units=metric`,
+        json: true
+      }).then((res) => {
+        callback(res);
+      }).catch((err) => {
+        console.log(err);
+        callback({ error: 'Could not reach OpenWeatherMap API.' });
+      });
+    }
+}
+  
+module.exports = Weather;
