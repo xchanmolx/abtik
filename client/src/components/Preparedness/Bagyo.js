43df2dfcb7e587afe4e52032b48f45a71e97a9de
xchanmolx <cmanigos@gmail.com>
2020-01-07 18:20:56 +0800

    update info

--- client/src/components/Preparedness/Bagyo.js
+++ client/src/components/Preparedness/Bagyo.js
@@ -30,10 +30,29 @@ const Bagyo = (props) => {
                 <p>6. Pag-dala ug bisti, pagkaon ug tambal kung mobakwit.</p>
                 <p><strong>(5) Lima ka pahimangno sa panahon sa bagyo!</strong></p>
                 <p>1. Mag-balantayon sa mga delikadong mga butang.</p>
+                <Alert color="info">
+                    Mga butang na mamahimong sinugdan sa kayo o di kaha mga butang
+                    nga delikado ibutang sa luwas ug uga nga butanganan.
+                </Alert>
                 <p>2. Ayaw pag-tunob ug baha ug hitak nga lugar.</p>
+                <Alert color="info">
+                    Likayi ang pag-basa bisan asa parte sa lawas aron pag likay sa nag kadaiyang
+                    sakit nga atong makuha sa baha ug aron usab pag likay nga dili ka masamad.
+                </Alert>
                 <p>3. Pag-pundo lang sa sulod sa panimalay.</p>
+                <Alert color="info">
+                    Pag-pundo lang sa sulod sa panimalay aron pag-likay sa mga mang lupad
+                    sa panahon sa bagyo nga mamahimong makadaut ug maka-disgrasya.
+                </Alert>
                 <p>4. Kanunay mag-subay sa mga balita bahin sa panahon.</p>
+                <Alert color="info">
+                    Paminaw sa mga balita bahin sa kahimtang sa panahon labi na diha sa inyong lugar.
+                </Alert>
                 <p>5. Kung hugaw ang tubig, pabukala kini sulod sa 20 minutos.</p>
+                <Alert color="info">
+                    Kung ang pundo nga tubig mainom dili na luwas, pabukalan kini ug hipusa sa usa ka
+                    sudlanan nga adunay taklob.
+                </Alert>
                 <p><strong>(4) Upat ka pahimangno sa humana ang bagyo!</strong></p>
                 <p>1. Sigurado-a nga luwas ang panimalay ug walay makasulod nga mga malang mananap.</p>
                 <p>2. Pag-bantay sa mga kable sa kuryente ug poste.</p>
