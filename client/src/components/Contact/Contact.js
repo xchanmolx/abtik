cb53797c5741138867b22f5aaaf99fda57392be3
xchanmolx <cmanigos@gmail.com>
2020-01-08 20:28:57 +0800

    update contact

--- client/src/components/Contact/Contact.js
+++ client/src/components/Contact/Contact.js
@@ -3,7 +3,13 @@ import { Random } from 'react-animated-text';
 
 import classes from './Contact.module.css';
 import Footer from '../Navigation/Footer/Footer';
-import { Alert } from 'reactstrap';
+import { 
+    Alert,
+    Toast,
+    ToastHeader,
+    ToastBody,
+    Badge
+} from 'reactstrap';
 
 const TextRandom = () => (
     <Random 
@@ -16,18 +22,35 @@ const TextRandom = () => (
 
 const Contact = () => (
     <div className={classes.Contact}>
-        <Alert color="info">
-            <h1><TextRandom /></h1>
+        <Alert color="warning" style={{textAlign: 'center'}}>
+            <h1><TextRandom /></h1>            
         </Alert>
-        <h6>Mobile Phone</h6>
-        <p>0926 530 7338 - TM</p>
-        <p>0922 238 8140 - SUN</p>
-        <hr />
-        <h6>Telephone</h6>
-        <p>469 9041</p>
-        <hr />
-        <h6>Email Address</h6>
-        <p>aloguinsandrrmo@gmail.com</p>
+        <Alert color="danger" style={{textAlign: 'center'}}>
+            <h3>3rd District Emergency Numbers</h3>
+        </Alert>
+        <div className="p-3 bg-info my-2 rounded">
+            <Toast>
+            <ToastHeader>
+                <h4>ALOGUINSAN</h4>
+            </ToastHeader>
+            <ToastBody>
+            <Badge color="warning"><h6>Rescue - ABTIK</h6></Badge>
+            <h6>Mobile Phone</h6>
+                0926-530-7338
+                <p>0922-238-8140</p>
+            <h6>Telephone</h6>
+                <p>032-469-9041</p>
+            <h6>Email Address</h6>
+                <p>aloguinsandrrmo@gmail.com</p>
+            <hr />
+            <Badge color="warning"><h6>POLICE</h6></Badge>
+            <h6>Mobile Phone</h6>
+                <p>0908-916-7215</p>
+            <h6>Telephone</h6>
+                032-469-9909<br />
+            </ToastBody>
+            </Toast>
+        </div>
         <Footer />
     </div>
 );
