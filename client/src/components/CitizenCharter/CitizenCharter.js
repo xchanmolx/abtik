dfcc772d1453d2be2bd70ad43b4cd18fbaecf58f
xchanmolx <cmanigos@gmail.com>
2020-02-06 14:11:27 +0800

    Update Citizen's Charter 02/06/2020 mm/dd/yyyy

--- client/src/components/CitizenCharter/CitizenCharter.js
+++ client/src/components/CitizenCharter/CitizenCharter.js
@@ -1,8 +1,16 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Random } from 'react-animated-text';
 
 import {
-    Alert   
+    Collapse,
+    Button,
+    CardBody,
+    Card,
+    Alert,
+    Container,
+    Row,
+    Col,
+    Table
 } from 'reactstrap';
 
 import classes from './CitizenCharter.module.css';
@@ -17,20 +25,72 @@ const TextRandom = () => (
     effectChange={2.2} />
 );
 
-class CitizenCharter extends Component {    
+const CitizenCharter = (props) => {    
+    const [isOpen, setIsOpen] = useState(false);
 
-    render () {
-        return (
-                <div className={classes.CitizenCharter}>                      
-                    <div className="p-3 bg-info my-2 rounded">
-                        <Alert color="secondary" >
-                            <h3><TextRandom /></h3>
-                        </Alert>
-                    </div>
+    const toggle = () => setIsOpen(!isOpen);
+
+    return (
+        // <div className={classes.CitizenCharter}>
+            
+                <Container className={classes.CitizenCharter}>
+                    <Alert color="warning" style={{textAlign: 'center'}}>
+                    <h2><TextRandom /></h2>
+                    </Alert>
+                    <Button color="info" onClick={toggle} style={{ marginBottom: '1rem' }}>click to SERVICES</Button>
+                    <Collapse isOpen={isOpen}>
+                    <Card>
+                    <CardBody>
+                    <Row>
+                        <Col sm="12">
+                            <Table>
+                                <thead>
+                                    <tr>
+                                    <th>FRONTLINE<br /> SERVICE</th>
+                                    <th>ACTIVITY<br /><p></p></th>
+                                    <th>RESPONSE<br /> TIME</th>
+                                    <th>PERSON/S<br /> RESPONSIBLE</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                            <td>Request for Data,<br /> Technical Assistance,<br /> Training and Services</td>
+                                            <td>Submit Request Letter</td>
+                                            <td>1 minute</td>
+                                            <td>Client</td>
+                                    </tr>
+                                    <tr>
+                                            <td>Jacob</td>
+                                            <td>Thornton</td>
+                                            <td>@fat</td>
+                                            <td>@mdo</td>
+
+                                    </tr>
+                                    <tr>
+                                            <td>Larry</td>
+                                            <td>the Bird</td>
+                                            <td>@twitter</td>
+                                            <td>@mdo</td>
+
+                                    </tr>
+                                    <tr>
+                                            <td>Larry</td>
+                                            <td>the Bird</td>
+                                            <td>@twitter</td>
+                                            <td>@mdo</td>
+                                    </tr>
+                                </tbody>
+                            </Table>
+                        </Col>
+                    </Row>
+                    </CardBody>
+                    </Card>
+                    </Collapse>
                     <Footer />
-                </div>
-        );
-    }    
+                </Container>
+                
+    );
+        
 }    
 
 export default CitizenCharter;
